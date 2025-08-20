cdd0dc5cd0448c4bff4e1492ecb4f77c628fd6cd
rwdled <79921530+rwdled@users.noreply.github.com>
2025-08-20 09:52:08 -0500

    Create Inventory.js

---
+++ my-app/src/Inventory.js
@@ -0,0 +1,10 @@
+const books = [
+    { title: "Effective Java", author: "Joshua Bloch", language: "Java" },
+    { title: "Java: The Complete Reference", author: "Herbert Schildt", language: "Java" },
+    { title: "Head First Java", author: "Kathy Sierra & Bert Bates", language: "Java" },
+    { title: "Eloquent JavaScript", author: "Marijn Haverbeke", language: "JavaScript" },
+    { title: "You Don’t Know JS", author: "Kyle Simpson", language: "JavaScript" },
+    { title: "JavaScript: The Good Parts", author: "Douglas Crockford", language: "JavaScript" }
+];
+
+export default books;
